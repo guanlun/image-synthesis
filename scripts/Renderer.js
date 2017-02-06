0f5ea9a0ba766894f64441b5be96dd7a40d063d1
Guanlun Zhao <guanlunzhao@gmail.com>
2017-02-06 10:29:03 -0600

    Added antialiasing option

--- scripts/Renderer.js
+++ scripts/Renderer.js
@@ -4,6 +4,33 @@ const Sphere = require('./Sphere');
 const Plane = require('./Plane');
 const Cylinder = require('./Cylinder');
 
+const antialiasing = true;
+
+const shapes = [
+    new Sphere(
+        new Vec3(-0.4, 0, 0),
+        0.3,
+        new Color(0.26, 0.53, 0.96)
+    ),
+    new Sphere(
+        new Vec3(0.2, 0.3, 0),
+        0.1,
+        new Color(0.95, 0.81, 0.26)
+    ),
+    new Plane(
+        new Vec3(0, 0, -0.3),
+        new Vec3(0.2, 1, 1),
+        new Color(0.65, 0.30, 0.76)
+    ),
+    new Cylinder(
+        new Vec3(0.3, 0.4, 0.6),
+        new Vec3(-0.5, 0.5, 1),
+        0.2,
+        1,
+        new Color(0.40, 0.81, 0.51)
+    ),
+];
+
 module.exports = class Renderer {
     constructor(canvasElement) {
         this.canvas = canvasElement;
@@ -12,75 +39,63 @@ module.exports = class Renderer {
     }
 
     render() {
-        console.log(new Color(1, 0, 0).toHexString());
-        const shapes = [
-            new Sphere(
-                new Vec3(0, 0, 0),
-                0.5,
-                new Color(1, 0, 0)
-            ),
-            new Plane(
-                new Vec3(0, 0, -0.3),
-                new Vec3(0.2, 1, 1),
-                new Color(0, 0, 1)
-            ),
-            new Cylinder(
-                new Vec3(0.5, 0.5, 0.5),
-                new Vec3(-0.5, 0.5, 1),
-                0.2,
-                1,
-                new Color(0, 1, 0)
-            ),
-        ];
-
-        const zPos = 0;
-
-        const crossPlaneWidth = 2;
-        const crossPlaneHeight = crossPlaneWidth / this.canvas.width * this.canvas.height;
-
-        const ratio = crossPlaneWidth / this.canvas.width;
-
         for (let y = 0; y < this.canvas.height; y++) {
             for (let x = 0; x < this.canvas.width; x++) {
                 const xRand = Math.random() * 0.25;
                 const yRand = Math.random() * 0.25;
 
-                let r = 0, g = 0, b = 0;
+                let resultColor;
 
-                for (let yOffset = 0; yOffset < 0.99; yOffset += 0.25) {
-                    for (let xOffset = 0; xOffset < 0.99; xOffset += 0.25) {
-                        const xJitterPos = x + xOffset + xRand;
-                        const yJitterPos = y + yOffset + yRand;
+                if (antialiasing) {
+                    let r = 0, g = 0, b = 0;
 
-                        const xPos = -crossPlaneWidth / 2 + xJitterPos * ratio;
-                        const yPos = -crossPlaneHeight / 2 + yJitterPos * ratio;
+                    for (let yOffset = 0; yOffset < 0.99; yOffset += 0.25) {
+                        for (let xOffset = 0; xOffset < 0.99; xOffset += 0.25) {
+                            const xJitterPos = x + xOffset + xRand;
+                            const yJitterPos = y + yOffset + yRand;
 
-                        const p = new Vec3(xPos, yPos, zPos);
+                            const sampleColor = this._computeColorAtPos(xJitterPos, yJitterPos);
 
-                        let sampleColor = null;
-
-                        for (let shapeIdx = 0; shapeIdx < shapes.length; shapeIdx++) {
-                            const shape = shapes[shapeIdx];
-
-                            if (shape.pointInside(p)) {
-                                sampleColor = shape.color;
-                                break;
+                            if (sampleColor) {
+                                r += sampleColor.r;
+                                g += sampleColor.g;
+                                b += sampleColor.b;
                             }
                         }
-
-                        if (sampleColor !== null) {
-                            r += sampleColor.r;
-                            g += sampleColor.g;
-                            b += sampleColor.b;
-                        }
                     }
-                }
 
-                const resultColor = new Color(r / 16, g / 16, b / 16);
+                    resultColor = new Color(r / 16, g / 16, b / 16);
+                } else {
+                    resultColor = this._computeColorAtPos(x, y);
+                }
 
                 this.context.fillStyle = resultColor.toHexString();
                 this.context.fillRect(x, y, 1, 1);
             }
         }
+    }
+
+    _computeColorAtPos(x, y) {
+        const crossPlaneWidth = 2;
+        const crossPlaneHeight = crossPlaneWidth / this.canvas.width * this.canvas.height;
+
+        const ratio = crossPlaneWidth / this.canvas.width;
+
+        const zPos = 0;
+
+        const xPos = -crossPlaneWidth / 2 + x * ratio;
+        const yPos = -crossPlaneHeight / 2 + y * ratio;
+
+        const p = new Vec3(xPos, yPos, zPos);
+
+        for (let shapeIdx = 0; shapeIdx < shapes.length; shapeIdx++) {
+            const shape = shapes[shapeIdx];
+
+            if (shape.pointInside(p)) {
+                return shape.color;
+            }
+        }
+
+        return new Color(0, 0, 0);
     }
 }
